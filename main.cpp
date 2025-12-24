ed0dcf1b1b51b56de4f609b83f2adda69e38661a
OverMoon64 <1598395112@qq.com>
2025-12-24 22:43:12 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "Level_Manage.h"
+#include "Game_Run.h"
+
+#include <iostream>
+
+using namespace std;
+
+// Level_Manage.h declares: extern int pf[5];
+// pf[0] acts like the "unlock" for level 1 (chooselevel checks pf[level-1]),
+// so we keep pf[0] = 1 by default.
+int pf[5] = {1, 0, 0, 0, 0};
+
+int main() {
+    cout << "Welcome to the game, nice to see you again~~~" << endl;
+
+    while (true) {
+        showlevel();
+        int levelnumber = chooselevel();
+        displayLevelDetails(levelnumber);
+        LevelInfo current_info = levelData[levelnumber - 1];
+        levelrun(levelnumber, current_info);
+    }
+
+    return 0;
+}
